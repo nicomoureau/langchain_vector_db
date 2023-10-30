86c4a7d7568c5d1ff00018c660352b374d8e334f
Nicolas Moureau <149340559+nicomoureau@users.noreply.github.com>
2023-10-29 20:49:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import pinecone
+
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.vectorstores import Pinecone
 from langchain.chains import RetrievalQA
-import pinecone
 
 
 pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment="gcp-starter")
@@ -21,13 +22,14 @@ if __name__ == "__main__":
     
     #print(len(texts))
     
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPEN_API_KEY"))
+    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPEN_API_KEY"))
     
     docsearch = Pinecone.from_documents(texts, embeddings, index_name="blogs-embeddings-index")
     
     qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
    
     query="What is a vector database? Give me a 15 word answer for a beginner."
+    
     result = qa.run(query)
     
     print(query,result)
